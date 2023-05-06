bcca2ecd32934e7d147bc1c4ee39d3ed72f77b3b
Andressa Guedes <andressajuliagr@hotmail.com>
2023-05-06 13:40:45 -0300

    Commit com codigo inicial Funcionalidade Login

---
+++ MyClosetWeb Codigo/MyClosetWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyClosetWeb.Models;
+using System.Diagnostics;
+
+namespace MyClosetWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterPage()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MyClosetWeb Codigo/MyClosetWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyClosetWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
